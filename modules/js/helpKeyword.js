4de2a4f333f383cfe1556f999afb72de642eafbe
Manoj <manoj.kumar@kony.com>
2013-03-14 12:54:58 +0530

    Initial checkin

---
+++ modules/js/helpKeyword.js
@@ -0,0 +1,130 @@
+/*
+****************************************************************
+*	Name    : searchByHelpKeyword
+*	Author  : Kony Solutions
+*	Purpose : This function is used to invoke the helpkeyword_mysql javaservice using appmiddlewareinvokerasync method  .
+****************************************************************
+*/
+
+		function searchByHelpKeyword() {
+			gNoOfRows = 0;
+			gPageno = 0;
+			gFromRow = 0;
+		    var mysqlhelpkeyword_inputparam = {};
+		    mysqlhelpkeyword_inputparam["serviceID"] = "helpkeyword_mysql";
+		    mysqlhelpkeyword_inputparam["httpheaders"] = {};
+		    mysqlhelpkeyword_inputparam["httpconfigs"] = {};
+		    kony.application.showLoadingScreen("loadskin1","Loading...",constants.LOADING_SCREEN_POSITION_FULL_SCREEN, true,true,null);
+		    var mysqlhelpkeyword = appmiddlewareinvokerasync(mysqlhelpkeyword_inputparam, helpKeywordCallback);        
+		};
+
+/*
+****************************************************************
+*	Name    : helpKeywordCallback
+*	Author  : Kony Solutions
+*	Purpose : This function is used to get parameters status & resultTable i.e. called when appmiddlewareinvokerasync method executes.
+****************************************************************
+*/
+		
+		function helpKeywordCallback(status, mysqlHelpKeywordData){
+		   if (status == 400) {
+		        if (mysqlHelpKeywordData["opstatus"] == 0) {
+		            if (mysqlHelpKeywordData != null && mysqlHelpKeywordData != undefined && mysqlHelpKeywordData["helpKeyword"] != null && mysqlHelpKeywordData["helpKeyword"] != undefined) {
+		                ghkData = [];
+		                for (var i = 0; i < mysqlHelpKeywordData["helpKeyword"].length; i++) {
+		                    ghkData.push({
+		                        "hk_id": mysqlHelpKeywordData["helpKeyword"][i]["hkid"],
+		                        "lblHelpKeyword": mysqlHelpKeywordData["helpKeyword"][i]["hkname"]
+		                    })
+		                }
+		                frmKeywords.lblInfo.text = "Select keyword for help topic : ";
+		                hbxFooterPage.setVisibility(true);
+		                gNoOfPages = Math.ceil((ghkData.length)/8);		                            
+		                paginationNext();
+		            }		       
+				}
+				else{
+					alert("Cannot find host on this network connection,Please check network & try again.");
+					frmKeywords.lblInfo.text = "Cannot find host on this network connection,Please check network & try again.";
+					hbxFooterPage.setVisibility(false);	
+		           	kony.application.dismissLoadingScreen();
+		           	return;
+				}
+			}
+		
+		}
+		
+
+		
+	// Defined onClick of next button for pagination of keywords data object in frmKeyword form.
+	
+		function paginationNext(){			
+			hbxFooterPage.vbxLeftFooter.hbxLeftFooter.imgLeftfooter.src = "arwleftd.png";
+			hbxFooterPage.vbxRightFooter.hbxRightFooter.imgRightfooter.src = "arwrightd.png";	
+			gPageno = gPageno+1;	
+			var absPageno = Math.abs(gPageno);			
+			var absNoOfPages = Math.abs(gNoOfPages);
+			if(absPageno > absNoOfPages){
+				hbxFooterPage.vbxRightFooter.hbxRightFooter.imgRightfooter.src = "trans1.png";
+				gPageno = gPageno-1;
+			 	return;
+			 }
+			gNoOfRows = gNoOfRows+8;
+			var absNoofRows = Math.abs(gNoOfRows);
+			if(absPageno == absNoOfPages)
+				hbxFooterPage.vbxRightFooter.hbxRightFooter.imgRightfooter.src = "trans1.png";
+			if(absPageno == 1)
+				hbxFooterPage.vbxLeftFooter.hbxLeftFooter.imgLeftfooter.src = "trans1.png";
+			else
+				gFromRow = gFromRow+8;	
+			hbxFooterPage.lblFrom.text = gPageno.toPrecision();
+			hbxFooterPage.lblTo.text = gNoOfPages.toPrecision();
+			var nextHelpKeywordData = [];
+			for (var i = gFromRow ; i < gNoOfRows; i++ ){
+				if(ghkData[i]!= null && ghkData[i]!= undefined)
+					nextHelpKeywordData.push(ghkData[i]);
+			}
+			frmKeywords.segHelpKeyword.setData(nextHelpKeywordData);
+			frmKeywords.show();
+			kony.application.dismissLoadingScreen();
+		}
+		
+	// Defined onClick of previous button for pagination of keywords data object in frmKeyword form.
+		
+		function paginationPrevious()
+		{
+			gPageno = gPageno-1;
+			hbxFooterPage.vbxLeftFooter.hbxLeftFooter.imgLeftfooter.src = "arwleftd.png";
+			hbxFooterPage.vbxRightFooter.hbxRightFooter.imgRightfooter.src = "arwrightd.png";	
+			var absPageno = Math.abs(gPageno);
+			if(absPageno < 1){
+				hbxFooterPage.vbxLeftFooter.hbxLeftFooter.imgLeftfooter.src = "trans1.png";
+				gPageno = gPageno+1;
+			 	return;
+			 }
+			if(absPageno == 1)
+				hbxFooterPage.vbxLeftFooter.hbxLeftFooter.imgLeftfooter.src = "trans1.png";
+			hbxFooterPage.lblFrom.text = gPageno.toPrecision();
+			gFromRow = gFromRow-8;
+			gNoOfRows = gNoOfRows-8;		
+			var preHelpKeywordData = [];
+			for(var i = gFromRow ; i < gNoOfRows ; i++){
+				if(ghkData[i]!= null && ghkData[i]!= undefined)
+					preHelpKeywordData.push(ghkData[i]);
+			}
+			frmKeywords.segHelpKeyword.setData(preHelpKeywordData);
+			frmKeywords.show();	
+			kony.application.dismissLoadingScreen();
+		}
+		
+	// To navigate frmKeywords form
+	
+		function navToFrmKeyword(){	
+			if(frmKeywords.lblInfo.text == "Cannot find host on this network connection,Please check network & try again.")			{
+				
+				searchByHelpKeyword()
+			}
+			else frmKeywords.show();
+						
+		}
+	
